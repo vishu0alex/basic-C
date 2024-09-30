aef7663740caf7309e7750369aa3b0fcc8670220
vishu0alex <vikas8146033487@gmail.com>
2024-09-30 13:58:06 +0530

    Create checkOddEven.cpp

---
+++ checkOddEven.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("enter a no. to check whether it is odd or even");
+	scanf("%d",&a);
+	if(a%2==0)
+	printf("given no. is even");
+	else 
+	printf("given no. is odd");
+
+	
+  return 0;
+	
+}

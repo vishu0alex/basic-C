87c9b29d9155bfaf8e1f8906853d951290bb8e41
vishu0alex <vikas8146033487@gmail.com>
2024-09-30 22:43:05 +0530

    Create binaryToDec.cpp

---
+++ binaryToDec.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+int main()
+{
+	char binary[64];
+	double decimal=0;
+	int length;
+	printf("enter a binary no.\n");
+	scanf("%s",binary);
+
+	length=strlen(binary);
+
+	for (int i = 0; i < length; i++)
+	{
+		if (binary[length-1-i]=='1')
+		{
+			decimal+=pow(2,i);
+		}
+	}
+	
+printf("decimal value=%lf\n",decimal);
+
+
+  return 0;
+	
+}

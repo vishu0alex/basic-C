688b89d1ebfcdd3163edf15bf7904a111c6cc2bc
vishu0alex <vikas8146033487@gmail.com>
2024-11-04 23:10:51 +0530

    Update checkprime.cpp

--- checkprime.cpp
+++ checkprime.cpp
@@ -6,11 +6,12 @@ int main()
 	scanf("%d",&n);
 	if(n==1||n==0)
 	flag=0;
-	for(int i=1;i<=n;i++)
+	else
+	{for(int i=1;i<=n;i++)
 	{
 		if(n%i==0)
 		flag++;
-	}
+	}}
 	if(flag==2)
 	printf("no. is prime");
 	else

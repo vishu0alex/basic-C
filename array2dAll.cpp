48c8073e2a8e17338c2da553001b01b173700a35
vishu0alex <vikas8146033487@gmail.com>
2024-11-19 16:28:37 +0530

    Update array2dAll.cpp

--- array2dAll.cpp
+++ array2dAll.cpp
@@ -49,6 +49,17 @@ void reverse(int arr[][100],int row, int col)
 	print(arr2,row2,col2);
 }
 
+void multiply(int arr[][100],int arr2[][100],int row, int col)
+{
+    int arr3[100][100]={0},row3=row,col3=col;
+	for(int i=0;i<row;i++)
+	{
+		for(int j=0;j<col;j++)
+		{
+		    arr3[i][j]=arr[i][j]*arr2[j][i];
+		}
+	}
+	print(arr3,row3,col3);
 
 int main()
 {
@@ -61,13 +72,104 @@ int main()
 	input(arr,row,col);
 	printf("enter the element of matrix 2\n");
 	input(arr2,row,col);
+	printf("matrix 1= \n");
     print(arr,row,col);
-    printf("\n");
+    printf("matrix 2= \n");
     print(arr2,row,col);
     printf("sum = \n");
     sum(arr,arr2,row,col);
     printf("reverse = \n");
     reverse(arr,row,col);
+    printf("multiply = \n");
+    multiply(arr,arr2,row,col);
+   
+	
+    return 0;	
+}
+#include<stdio.h>
+void input(int arr[][100],int row, int col)
+{
+	for(int i=0;i<row;i++)
+	{
+		for(int j=0;j<col;j++)
+		{
+			scanf("%d",&arr[i][j]);
+		}
+	}
+}
+
+
+void print(int arr[][100],int row, int col)
+{
+	for(int i=0;i<row;i++)
+	{
+		for(int j=0;j<col;j++)
+		{
+			printf("%d ",arr[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void sum(int arr[][100],int arr2[][100],int row, int col)
+{
+    int arr3[100][100]={0},row3=row,col3=col;
+	for(int i=0;i<row;i++)
+	{
+		for(int j=0;j<col;j++)
+		{
+		    arr3[i][j]=arr[i][j]+arr2[i][j];
+		}
+	}
+	print(arr3,row3,col3);
+}
+
+void reverse(int arr[][100],int row, int col)
+{
+    int arr2[100][100]={0},row2=row,col2=col;
+	for(int i=0;i<row;i++)
+	{
+		for(int j=0;j<col;j++)
+		{
+		    arr2[i][j]=arr[j][i];
+		}
+	}
+	print(arr2,row2,col2);
+}
+
+void multiply(int arr[][100],int arr2[][100],int row, int col)
+{
+    int arr3[100][100]={0},row3=row,col3=col;
+	for(int i=0;i<row;i++)
+	{
+		for(int j=0;j<col;j++)
+		{
+		    arr3[i][j]=arr[i][j]*arr2[j][i];
+		}
+	}
+	print(arr3,row3,col3);
+
+int main()
+{
+	int row,col;
+	int arr[100][100],arr2[100][100];
+	printf("enter row and column \n");
+	scanf("%d%d",&row,&col);
+	
+	printf("enter the element of matrix 1\n");
+	input(arr,row,col);
+	printf("enter the element of matrix 2\n");
+	input(arr2,row,col);
+	printf("matrix 1= \n");
+    print(arr,row,col);
+    printf("matrix 2= \n");
+    print(arr2,row,col);
+    printf("sum = \n");
+    sum(arr,arr2,row,col);
+    printf("reverse = \n");
+    reverse(arr,row,col);
+    printf("multiply = \n");
+    multiply(arr,arr2,row,col);
    
 	
     return 0;	
